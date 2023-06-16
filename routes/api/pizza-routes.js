6c604758c68580ab0f957df9c1a6f1e6030adb1b
bokigolic <bokigolic32@gmail.com>
2023-06-15 23:02:14 -0400

    wokr on apis and routes

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+// Set up GET all and POST at /api/pizzas
+router
+  .route('/')
+  .get()
+  .post();
+
+// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+router
+  .route('/:id')
+  .get()
+  .put()
+  .delete();
+
+// /api/pizzas
+router
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
+
+  // /api/pizzas/:id
+router
+.route('/:id')
+.get(getPizzaById)
+.put(updatePizza)
+.delete(deletePizza);
+
+module.exports = router;
